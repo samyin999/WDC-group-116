073b03021ca2bde47635e21155dec51b718f7b09
samyin999 <samyin999@gmail.com>
2021-06-11 11:48:11 +0930

    finished user.html

--- public/javascripts/user.js
+++ public/javascripts/user.js
@@ -11,19 +11,84 @@ function getDate() {
 function homePress() {
     document.getElementById('checkInActive').style.display = "none";
     document.getElementById('checkInInactive').style.display = "block";
+    document.getElementById('mapActive').style.display = "none";
+    document.getElementById('mapInactive').style.display = "block";
+    document.getElementById('historyActive').style.display = "none";
+    document.getElementById('historyInactive').style.display = "block";
+    document.getElementById('accountActive').style.display = "none";
+    document.getElementById('accountInactive').style.display = "block";
 
     document.getElementById('userHome').style.display = "block";
+    document.getElementById('userCheckIn').style.display = "none";
+    document.getElementById('userMap').style.display = "none";
+    document.getElementById('userCheckHistory').style.display = "none";
+    document.getElementById('userAccount').style.display = "none";
 }
 
 function checkInPress() {
     document.getElementById('checkInActive').style.display = "block";
     document.getElementById('checkInInactive').style.display = "none";
+    document.getElementById('mapActive').style.display = "none";
+    document.getElementById('mapInactive').style.display = "block";
+    document.getElementById('historyActive').style.display = "none";
+    document.getElementById('historyInactive').style.display = "block";
+    document.getElementById('accountActive').style.display = "none";
+    document.getElementById('accountInactive').style.display = "block";
 
     document.getElementById('userHome').style.display = "none";
-    document.getElementById('userCheckIn').style.disply = "block";
+    document.getElementById('userCheckIn').style.display = "block";
+    document.getElementById('userMap').style.display = "none";
+    document.getElementById('userCheckHistory').style.display = "none";
+    document.getElementById('userAccount').style.display = "none";
 }
 
 function mapPress() {
+    document.getElementById('checkInActive').style.display = "none";
+    document.getElementById('checkInInactive').style.display = "block";
+    document.getElementById('mapActive').style.display = "block";
+    document.getElementById('mapInactive').style.display = "none";
+    document.getElementById('historyActive').style.display = "none";
+    document.getElementById('historyInactive').style.display = "block";
+    document.getElementById('accountActive').style.display = "none";
+    document.getElementById('accountInactive').style.display = "block";
 
+    document.getElementById('userHome').style.display = "none";
+    document.getElementById('userCheckIn').style.display = "none";
+    document.getElementById('userMap').style.display = "block";
+    document.getElementById('userCheckHistory').style.display = "none";
+    document.getElementById('userAccount').style.display = "none";
 }
 
+function historyPress() {
+    document.getElementById('checkInActive').style.display = "none";
+    document.getElementById('checkInInactive').style.display = "block";
+    document.getElementById('mapActive').style.display = "none";
+    document.getElementById('mapInactive').style.display = "block";
+    document.getElementById('historyActive').style.display = "block";
+    document.getElementById('historyInactive').style.display = "none";
+    document.getElementById('accountActive').style.display = "none";
+    document.getElementById('accountInactive').style.display = "block";
+
+    document.getElementById('userHome').style.display = "none";
+    document.getElementById('userCheckIn').style.display = "none";
+    document.getElementById('userMap').style.display = "none";
+    document.getElementById('userCheckHistory').style.display = "block";
+    document.getElementById('userAccount').style.display = "none";
+}
+
+function accountPress() {
+    document.getElementById('checkInActive').style.display = "none";
+    document.getElementById('checkInInactive').style.display = "block";
+    document.getElementById('mapActive').style.display = "none";
+    document.getElementById('mapInactive').style.display = "block";
+    document.getElementById('historyActive').style.display = "none";
+    document.getElementById('historyInactive').style.display = "block";
+    document.getElementById('accountActive').style.display = "block";
+    document.getElementById('accountInactive').style.display = "none";
+
+    document.getElementById('userHome').style.display = "none";
+    document.getElementById('userCheckIn').style.display = "none";
+    document.getElementById('userMap').style.display = "none";
+    document.getElementById('userCheckHistory').style.display = "none";
+    document.getElementById('userAccount').style.display = "block";
+}
